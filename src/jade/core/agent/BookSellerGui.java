c6b21e9deb54828e53491568a123dd7462fc3124
Zhmaeva Julia <yuliia.zhmaieva@nure.ua>
2016-12-11 19:50:31 +0200

    first commit

---
+++ src/jade/core/agent/BookSellerGui.java
@@ -0,0 +1,15 @@
+package jade.core.agent;
+
+
+/**
+ * Created by Zhmaeva_Julia on 10.11.2016.
+ */
+public class BookSellerGui {
+
+    public void show() {
+    }
+
+    public void dispose() {
+
+    }
+}
